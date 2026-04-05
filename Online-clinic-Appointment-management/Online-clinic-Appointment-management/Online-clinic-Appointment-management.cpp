5a5b5e37bd3fdee019cf304bbb17d260f0d10149
Adham _Hany <domapes345@gmail.com>
2026-04-05 22:31:22 +0200

    Good work

--- Online-clinic-Appointment-management/Online-clinic-Appointment-management/Online-clinic-Appointment-management.cpp
+++ Online-clinic-Appointment-management/Online-clinic-Appointment-management/Online-clinic-Appointment-management.cpp
@@ -28,4 +28,53 @@ void ViewDoctors() {
 
 int main() {
 
+} 
+// Adham hany void bookAppointment() {
+    
+    
+    
+/* Appointment appointment;
+
+cout << "\t\t\tBook Appointment\n";
+cout << "\t\t\t#################\n\n";
+cout << "Please enter pataint ID:" << "  ";
+cin >> appointment.patientID;
+cout << "\n";
+cout << "Please enter doctor ID:" << "  ";
+cin >> appointment.doctorID;
+cout << "\n";
+bool check = false;
+for (int i = 0; i < MAX_DOCTORS; i++) {
+    if (doctors[i].doctorID == appointment.doctorID) {
+        check = true;
+        break;
+    }
 }
+if (check) {
+    cout << "Doctor is found\n\n ";
+    cout << "Please enter the date:" << "   ";
+    cin >> appointment.appointmentDate;
+    cout << "\n";
+    cout << "Please enter the time:" << "    ";
+    cin >> appointment.appointmentTime;
+    cout << "\n\n";
+    createAppointment(appointment.patientID, appointment.doctorID, appointment.appointmentDate, appointment.appointmentTime);
+    cout << "\t\t(Appointment Booked successfully)\n";
+}
+else cout << "Doctor ID not found\n";
+
+}
+void createAppointment(int patient_ID, int Doctor_ID, string Date, string Time) {
+    if (appointmentCount < MAX_APPOINTMENTS) {
+
+        appointments[appointmentCount].patientID = patient_ID;
+        appointments[appointmentCount].doctorID = Doctor_ID;
+        appointments[appointmentCount].appointmentDate = Date;
+        appointments[appointmentCount].appointmentTime = Time;
+        appointmentCount++;
+    }
+    else cout << "Sorry,My friend,The clinic is full\n";
+
+
+} */
+
