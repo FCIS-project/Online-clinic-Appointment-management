b5b4a62d7ee0521c3fc9da9918bc8f08c2c834e5
Adham-Saeed630 <adhamsaaid630@gmail.com>
2026-04-05 20:19:11 +0200

    Pushing Project Code

---
+++ Online-clinic-Appointment-management/Online-clinic-Appointment-management/Online-clinic-Appointment-management.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+// Person 2
+void DisplayDoctors() {
+	if (doctorCount == 0) {
+		cout << "No Doctors Availble :( " << endl;
+	}
+	else {
+		for (int i = 0; i < 1; i++) {
+
+			Doctor Doctor = doctors[i];
+
+			cout << "Doctor ID: \t" << Doctor.DoctorID << endl;
+			cout << "Doctor Name: \t" << Doctor.name << endl;
+			cout << "Specialization: \t" << Doctor.specialization << endl;
+			cout << "availableSlots: \t" << Doctor.availableSlots << endl;
+			cout << "Consultantion Fee: \t" << Doctor.consultantionFee << endl;
+		};
+	};
+};
+
+void ViewDoctors() {
+	DisplayDoctors();
+};
+
+
+int main() {
+
+}
